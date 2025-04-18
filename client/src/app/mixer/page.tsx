4f3354c46f31b6360f4c1d6780ae1d1be16519e7
Rosey Angina <rozeyangina@icloud.com>
2025-04-18 00:24:21 -0400

    mixer/page.tsx

--- client/src/app/mixer/page.tsx
+++ client/src/app/mixer/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = 'force-dynamic';
+
 import Navbar from "../../../components/Navbar/Navbar";
 import AudioMixer from "../../../components/AudioMixer/AudioMixer";
 import "./mixer.css"; 
