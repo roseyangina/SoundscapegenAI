0d7fc72c2f61cac62bc90bb9cf863d2117a6d38e
Rosey Angina <rozeyangina@icloud.com>
2025-04-18 00:28:56 -0400

    fix: force dynamic rendering for /mixer to support useSearchParams

--- client/src/app/mixer/page.tsx
+++ client/src/app/mixer/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic'; //linting
 
 import Navbar from "../../../components/Navbar/Navbar";
 import AudioMixer from "../../../components/AudioMixer/AudioMixer";
